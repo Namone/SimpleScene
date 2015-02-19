27b33b701e66b3f3d5758fc89ae02910cc9643eb
David Jeske <davidj@gmail.com>
2015-02-18 16:27:42 -0800

    clean up and prepare for average normal calculations

--- Example2DTileGame/SSMap.cs
+++ Example2DTileGame/SSMap.cs
@@ -75,7 +75,7 @@ namespace Example2DTileGame
             float totalHeight;
 
 
-            // Store heights \\
+            // Store heights
             for(int i = 0; i < mapHeight.GetLength(0) - 1; i++)
             {
                 for(int j = 0; j < mapHeight.GetLength(1) - 1; j++)
@@ -84,7 +84,7 @@ namespace Example2DTileGame
                 }
             }
 
-            // Relax the data \\
+            // Relax the data 
             {
                 float[,] tHeights = new float[arrayW, arrayH];
                 float h1;
@@ -116,7 +116,7 @@ namespace Example2DTileGame
 
             }
 
-            // Draw the mesh \\
+            // construct the 3d mesh data
             for (int i = 1; i < mapHeight.GetLength(0) - 1; i++)
             {
                 for (int j = 1; j < mapHeight.GetLength(1) - 1; j++)
@@ -130,60 +130,12 @@ namespace Example2DTileGame
                     p2 = new Vector3(squareCX, average2x2(i, j + 1), squareCY + squareWidth);
                     p3 = new Vector3(squareCX + squareWidth, average2x2(i + 1, j + 1), squareCY + squareWidth);
 
-                    storeNormals(p0, p1, p2, p3);
-
                     middle = new Vector3(squareCX + Middle, mapHeight[i, j], squareCY + Middle);
         
                     addToMapArray(p0, p1, p2, p3, middle);
                    
                 }
             }
-        }
-
-        /// <summary>
-        /// Draw map
-        /// </summary>
-        public void drawMap()
-        {
-
-            // Draw the 'outline' of the map
-            GL.Begin(PrimitiveType.Lines);
-            {
-
-                // Drawing code
-                foreach (VertexData v in groundMesh_Lines)
-                {
-                    GL.Color4(v.Color);
-                    GL.Vertex3(v.Pos); // Draw
-                }
-
-            }
-            GL.End();
-
-            // Draw the triangle 'ground'
-            GL.Begin(PrimitiveType.Triangles);
-            {
-                foreach (VertexData v in groundMesh_Tri)
-                {
-                    GL.Color4(v.Color);
-					GL.Vertex3(v.Pos);
-                }
-            }
-            GL.End();
-
-            // Draw the triangle 'ground'
-            GL.Begin(PrimitiveType.Lines);
-            {
-                foreach (VertexData v in groundMesh_Tri)
-                {
-					GL.Color4(Color4.Red);
-					GL.Vertex3(v.Pos);
-					GL.Vertex3(v.Pos + v.triangleFaceNormal.Normalized()); // Draw normalized vector
-                }
-            }
-            GL.End();
-
-
         }
 
         /// <summary>
@@ -197,17 +149,17 @@ namespace Example2DTileGame
         public void addToMapArray(Vector3 p0, Vector3 p1, 
             Vector3 p2, Vector3 p3, Vector3 middle)
         {
-            // TEMPORARLIY COMMENTED OUT
-            // Base
+            
+
+            // step 1. add lines to groundMesh_Lines
+
+			// ...box around the tile
             //groundMesh_Lines.Add(new VertexData(p0, colorForHeight(p0.Y))); groundMesh_Lines.Add(new VertexData(p1, colorForHeight(p1.Y)));  
-
             //groundMesh_Lines.Add(new VertexData(p0, colorForHeight(p0.Y))); groundMesh_Lines.Add(new VertexData(p2, colorForHeight(p2.Y)));
-
             //groundMesh_Lines.Add(new VertexData(p2, colorForHeight(p2.Y))); groundMesh_Lines.Add(new VertexData(p3, colorForHeight(p3.Y)));
-
             //groundMesh_Lines.Add(new VertexData(p3, colorForHeight(p3.Y))); groundMesh_Lines.Add(new VertexData(p1, colorForHeight(p1.Y)));
 
-            //// Middle
+            // ...lines to the middle
             //groundMesh_Lines.Add(new VertexData(p0, colorForHeight(p0.Y))); groundMesh_Lines.Add(new VertexData(middle, colorForHeight(middle.Y)));
             //groundMesh_Lines.Add(new VertexData(p1, colorForHeight(p1.Y))); groundMesh_Lines.Add(new VertexData(middle, colorForHeight(middle.Y)));
             //groundMesh_Lines.Add(new VertexData(p2, colorForHeight(p2.Y))); groundMesh_Lines.Add(new VertexData(middle, colorForHeight(middle.Y)));
@@ -216,30 +168,36 @@ namespace Example2DTileGame
 
             //----------------------------------------------------
 
-            //Triangles
+            // step 2. add Triangles to groundMesh_Tri
              
             // bottom-left : middle : top-left
-            groundMesh_Tri.Add(new VertexData(p0, colorForHeight(p0.Y), p0));
-            groundMesh_Tri.Add(new VertexData(middle, colorForHeight(middle.Y), p0));
-            groundMesh_Tri.Add(new VertexData(p1, colorForHeight(p1.Y), p0)); 
-
+			addGroundTriangle(p0,middle,p1);
 
 			// top-left : middle : top-right
-            groundMesh_Tri.Add(new VertexData(p1, colorForHeight(p1.Y), p0)); // 1
-            groundMesh_Tri.Add(new VertexData(middle, colorForHeight(middle.Y), p0)); // 2
-            groundMesh_Tri.Add(new VertexData(p3, colorForHeight(p3.Y), p0)); // 3
+			addGroundTriangle(p1,middle,p3);
 
 			// top-right : middle : bottom-right
-            groundMesh_Tri.Add(new VertexData(p3, colorForHeight(p3.Y), p0)); // 1
-            groundMesh_Tri.Add(new VertexData(middle, colorForHeight(middle.Y), p0)); // 2
-            groundMesh_Tri.Add(new VertexData(p2, colorForHeight(p2.Y), p0)); // 3
+			addGroundTriangle(p3,middle,p2);
 
             // bottom-right: middle : bottom-left
-            groundMesh_Tri.Add(new VertexData(p2, colorForHeight(p2.Y), p0)); // 1
-            groundMesh_Tri.Add(new VertexData(middle, colorForHeight(middle.Y), p0)); // 2
-            groundMesh_Tri.Add(new VertexData(p0, colorForHeight(p0.Y), p0)); // 3
-
+			addGroundTriangle(p2,middle,p0);
         }
+
+		
+		private void addGroundTriangle(Vector3 tp0, Vector3 tp1, Vector3 tp2) {
+			// compute the triangle normal
+			var triNormal = calcNormal(tp0,tp1,tp2);
+
+			// add the triangle to the ground mesh
+	        groundMesh_Tri.Add(new VertexData(tp0, colorForHeight(tp0.Y), triNormal));
+	        groundMesh_Tri.Add(new VertexData(tp1, colorForHeight(tp1.Y), triNormal));
+	        groundMesh_Tri.Add(new VertexData(tp2, colorForHeight(tp2.Y), triNormal)); 
+			
+			// accumulate the triangle normal for all three points.
+			storeNormal(tp0,triNormal);
+			storeNormal(tp1,triNormal);
+			storeNormal(tp2,triNormal);
+		}
 
         /// <summary>
         /// Calculate normals/store point values associated with each other
@@ -248,55 +206,25 @@ namespace Example2DTileGame
         /// <param name="p1">Point 2</param>
         /// <param name="p2">Middle</param>
         /// <returns></returns>
-        public void storeNormals(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+        private Vector3 calcNormal(Vector3 p0, Vector3 p1, Vector3 p2) 
+		{
+            Vector3 normal = Vector3.Cross(p0 - p1, p0 - p2);
+            return normal;
+		}
+
+
+		/// <summary>
+		/// store the triangle normal in the accumulation dictionary, indexed by position
+		/// </summary>
+		/// <param name="position">Position.</param>
+		/// <param name="triNormal">Tri normal.</param>
+        private void storeNormal(Vector3 position, Vector3 triNormal)
         {
-            // Store the map points\\
+			// step 1. find out if there is an entry positionToNormalList[position] 
+			
+			// step 2...if not, create an empty list and put it in positionToNormalList[position]
 
-            // Individual key, and every vertex that matches that key will be placed
-            // in a corrisponding list
-
-            // If the the current vertex is NOT already keyed into dictionary... then add it/them
-
-            //!remember!
-            // every p0, p1, p2, & p3, are different most of the time (except when they meet at a common point...)
-            // MEANING the keys change
-            if (!positionToNormalList.ContainsKey(p0) && !positionToNormalList.ContainsKey(p1)
-                && !positionToNormalList.ContainsKey(p2) && !positionToNormalList.ContainsKey(p3))
-            {
-                positionToNormalList.Add(p0, new List<Vector3> { p0 }); // Create it in dictionary and add to list
-                positionToNormalList.Add(p1, new List<Vector3> { p1 }); // Create it in dictionary and add to list
-                positionToNormalList.Add(p2, new List<Vector3> { p2 }); // Create it in dictionary and add to list
-                positionToNormalList.Add(p3, new List<Vector3> { p3 }); // Create it in dictionary and add to list    
-
-            }
-
-            // If the key already exists...
-            if (positionToNormalList.ContainsKey(p0))
-            {
-                for (int index = 0; index < positionToNormalList[p0].Count; index++)
-                {
-                    positionToNormalList[p0][index].Add(p0); // Add the new point to corrispoding list
-
-                    // TODO - repeat for others and test if working
-                }
-            }
-
-            else if (positionToNormalList.ContainsKey(p1))
-            {
-                
-            }
-
-            else if (positionToNormalList.ContainsKey(p2))
-            {
-                
-            }
-
-            else if (positionToNormalList.ContainsKey(p3))
-            {
-                
-            }
-
-
+            // step 3. add triNormal to the list
 
         }
 
@@ -309,13 +237,62 @@ namespace Example2DTileGame
         {
             base.Render(ref renderConfig);
 
-            // Set-up render
+            // step 1. Set-up render
             SSShaderProgram.DeactivateAll(); // Disable GLSL
             GL.Disable(EnableCap.Texture2D);
             GL.Disable(EnableCap.Lighting);
             // End of set-up
 
-            drawMap(); // Draw it
+            // step 2. Draw the wireframe 'outline' of the map
+            GL.Begin(PrimitiveType.Lines);
+            {
+                foreach (VertexData v in groundMesh_Lines) {
+                    GL.Color4(v.Color);
+                    GL.Vertex3(v.Pos); // Draw
+                }
+            }
+            GL.End();
+
+            // step 3. Draw the triangle 'ground' mesh
+            GL.Begin(PrimitiveType.Triangles);
+            {
+                foreach (VertexData v in groundMesh_Tri) {
+                    GL.Color4(v.Color);
+					GL.Vertex3(v.Pos);
+                }
+            }
+            GL.End();
+
+            // step 4. Draw the per-triangle triFaceNormal
+            GL.Begin(PrimitiveType.Lines);
+            {
+                foreach (VertexData v in groundMesh_Tri) {
+					GL.Color4(Color4.Red);
+					GL.Vertex3(v.Pos);
+					GL.Vertex3(v.Pos + v.triangleFaceNormal.Normalized()); // Draw normalized vector
+                }
+            }
+            GL.End();
+
+            // step 5. Draw all average normals from the dictionary positionToNormalList
+            GL.Begin(PrimitiveType.Lines);
+            {
+                foreach (var kvp in positionToNormalList) {
+					Vector3 position = kvp.Key;
+					List<Vector3> normalList = kvp.Value;
+					Vector3 avgNormal = new Vector3(0,0,0);
+					foreach (var normal in normalList) {
+						avgNormal += normal;
+					}
+					avgNormal /= normalList.Count;
+					avgNormal = avgNormal.Normalized();
+
+					GL.Color4(Color4.Blue);
+					GL.Vertex3(position);
+					GL.Vertex3(position + (avgNormal * 1.5f)); // Draw normalized vector
+                }
+            }
+            GL.End();
 
         }
         
