f13c2db34c08a3c1b6bfb745738d5aeae12491dd
Namone <al2xnord@yahoo.com>
2015-03-03 22:34:48 -0700

    Some beginning work on brush sizes and terraforming

--- Example2DTileGame/SSMap.cs
+++ Example2DTileGame/SSMap.cs
@@ -415,10 +415,23 @@ namespace Example2DTileGame
           
             Console.WriteLine("tileSpaceXY ({0},{1})  tileXY ({2},{3})", tileSpace_x,tileSpace_y,tile_x,tile_y);
 
-			// float brushRadius = 0.5f;
-		
-            // raise the actual map data
-			mapHeight[tile_x,tile_y]+=raiseAmount;
+			float brushRadius = 0.5f;
+			// raise the actual map data
+			mapHeight[tile_x, tile_y] += raiseAmount;
+
+			// This idea... but not done like this
+			/*mapHeight[tile_x + brushRadius, tile_y + brushRadius] += raiseAmount;
+			mapHeight[tile_x - brushRadius, tile_y - brushRadius] += raiseAmount;
+			mapHeight[tile_x - brushRadius, tile_y + brushRadius] += raiseAmount;
+			mapHeight[tile_x + brushRadius, tile_y - brushRadius] += raiseAmount;*/
+
+			for (int i = 0; i < mapHeight.GetLength (0); i++) {
+				for (int j = 0; j < mapHeight.GetLength (1); j++) {
+					// TODO - If statement determining if current
+					// x, y coordinate is within 'range' of click
+					// coordinate
+				}
+			}
 
             generateMeshFromHeightmap();
         }
