33d331b8d1f60df93fa0ac15aaca17fdebe9a579
David Jeske <davidj@gmail.com>
2015-03-13 15:23:37 -0700

    scaffold sprite sheet class

---
+++ Example2DTileGame/SpriteSheet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Example2DTileGame
+{
+    public class SpriteSheet
+    {
+        int gridWidth,gridHeight;
+
+        public SpriteSheet(int numTilesWide,int numTilesHigh) {
+            this.gridWidth = numTilesWide;
+            this.gridHeight = numTilesHigh;
+        }
+
+        public RectangleF getTileBounds(int tileID) {
+
+            // TODO: implement this
+
+            // given the tileID, calculate the opengl 0.0-1.0 coordinate space location of the bounds
+            // of that tile inside the sprite sheet
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
